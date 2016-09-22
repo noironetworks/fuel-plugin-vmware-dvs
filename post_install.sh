94b2c946880fb4be1d26109dcd621fe314bb878f
Ratnakar Kolli <rkolli@noironetworks.com>
2016-09-22 09:24:05 -0700

    changes for liberty

--- post_install.sh
+++ post_install.sh
@@ -15,11 +15,13 @@
 
 #/usr/bin/dockerctl shell nailgun sed -i -e "/if common_attrs.get('use_vcenter', {}).get('value') is True and/,+5 d" /usr/lib/python2.7/site-packages/nailgun/api/v1/validators/cluster.py
 #/usr/bin/dockerctl shell nailgun systemctl restart nailgun.service
-cd /var/www/nailgun/plugins/fuel-plugin-vmware-dvs-2.1
-dockerctl copy sqlpatch.py nailgun:/root/.
-dockerctl shell nailgun /root/sqlpatch.py
-dockerctl  copy cluster.patch nailgun:/root/.
-/usr/bin/dockerctl shell nailgun yum -y install patch
-dockerctl shell nailgun bash -c "/usr/bin/patch -p4 -N  /usr/lib/python2.6/site-packages/nailgun/api/v1/validators/cluster.py /root/cluster.patch" || true
-dockerctl  copy start.patch nailgun:/root/.
-dockerctl shell nailgun bash -c "/usr/bin/patch -p4 -N /usr/local/bin/start.sh /root/start.patch" || true
+
+#cd /var/www/nailgun/plugins/fuel-plugin-vmware-dvs-2.2
+#dockerctl copy sqlpatch.py nailgun:/root/.
+#dockerctl shell nailgun /root/sqlpatch.py
+
+#dockerctl  copy cluster.patch nailgun:/root/.
+#/usr/bin/dockerctl shell nailgun yum -y install patch
+#dockerctl shell nailgun bash -c "/usr/bin/patch -p4 -N  /usr/lib/python2.6/site-packages/nailgun/api/v1/validators/cluster.py /root/cluster.patch" || true
+#dockerctl  copy start.patch nailgun:/root/.
+#dockerctl shell nailgun bash -c "/usr/bin/patch -p4 -N /usr/local/bin/start.sh /root/start.patch" || true
